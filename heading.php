7cf79f18cd777b4dbd80f5588aab1d01f36fda99
hrantgartley <doyouevenjump@gmail.com>
2023-11-13 19:29:36 -0600

    js and css added

--- heading.php
+++ heading.php
@@ -8,6 +8,7 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Agbalumo&display=swap" rel="stylesheet">
+	<?php require('dropdown.php'); ?>
 	<div class="center">
 		<h1 style="padding-left: 5%">Electric cars through the ages</h1>
 	</div>
